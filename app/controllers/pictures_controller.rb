38e96281ae50abbd90659de6523608d8ae8419c4
zdbrenda <zdbrenda@gmail.com>
2021-11-06 19:12:56 -0600

    Add photo displaying features for a given user ID in index page, with environment variables properlly stored with figaro gem

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,12 +1,18 @@
 class PicturesController < ApplicationController
 
+require 'flickr'
+
 
 
     def index
         @pictures = Picture.all
         @search = params["search"]
-        if @searcg.present?
+        if @search.present?
             @id = @search["user_id"]
+            @flickr = Flickr.new(ENV["FLICKR_API_KEY"],ENV["FLICKR_SHARED_SECRET"])
+            if @flickr != nil
+                @returned_pictures = @flickr.people.getPublicPhotos(:user_id=>@id)
+            end
         end
 
     end
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the Rails application.
 require_relative "application"
 
+flickr_config = File.join(Rails.root, 'config', 'flickr_config.rb')
+load(flickr_config) if File.exist?(flickr_config)
+
 # Initialize the Rails application.
+
 Rails.application.initialize!
